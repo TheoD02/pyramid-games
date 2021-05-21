7edc57727eeff36ca7c6048d96939567c8a71aac
TheoD02 <theo.d02290@gmail.com>
2021-05-22 01:19:58 +0200

    qsd

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
 
 const server = express()
-	.use((req, res) => res.sendFile(INDEX, { root: __dirname + 'public/' }))
+	.use((req, res) => res.sendFile(INDEX, { root: __dirname + '/public/' }))
 	.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 const wss = new Server({ server });
